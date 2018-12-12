964dc14b3d1bfc2ded296d0e94ff209468cf79c3
tsscds <tsscds@4b1a781e-1fcd-455b-a5aa-50a18b27795a>
2018-12-12 16:50:36 +0000

    tsscds.sh and kmc.f90 update

--- src/kmc.f90
+++ src/kmc.f90
@@ -11,12 +11,12 @@ integer, parameter :: dp = selected_real_kind(15,307)
 integer :: m,nran,nr,nesp,inran,i,j,k,l,mu,ndisp,pd,kk,ijk,tcont,tint
 real(dp) :: t,a0,r2a0,suma,ptot
 real(dp) :: rnd(2)
-integer,dimension(:),allocatable :: p,p0,re,pr,n,ndd,cont
+integer,dimension(:),allocatable :: p,p0,re,pr,n,ndd,cont,pold,sig,sigold,rep
 real (dp) ,dimension(:),allocatable :: a,rate
 read(*,"(a80)") title
 print "(t3,a80)",title
 read(*,*) m,nesp,nran
-allocate(re(m),pr(m),a(m),rate(m),p0(nesp),p(nesp),n(nesp),cont(m))
+allocate(re(m),pr(m),a(m),rate(m),p0(nesp),p(nesp),n(nesp),cont(m),pold(nesp),sig(nesp),sigold(nesp),rep(nesp))
 n=(/ (l,l=1,nesp) /)
 do i=1,m
 read(*,*) rate(I),re(i),pr(i)
@@ -40,21 +40,25 @@ print "(t3,a,1p,20(e9.2))","Rates:",rate
 print "(t3,a,20(i9))","Reacts:",re
 print "(t3,a,20(i9))","Prods :",pr
 print "(/,t3,a,1p,i10)","Step size =",tint
-big: do inran=1,nran
-   print "(/,t3,a,i4,/,t3,a,9999(i7))","Calculation number",inran,"Time(ps)",n
+outter: do inran=1,nran
+   print "(/,t3,a,i4,/,t3,a,9999(i8))","Calculation number",inran,"Time(ps)",n
    p=p0
+   pold=p0
+   sig=p0
    t=0.d0
    tcont=0
-   print "(e10.4,9999(i7))",t,p
+   rep=0
+   print "(e10.4,9999(i8))",t,p
    do j=1,m
       a(j)=rate(j)*p(re(j))
    enddo
    a0=sum(a)
-   do while(a0>0)
+   inner: do while(a0>0)
      call random_number(rnd)
      t=t-log(rnd(1))/a0      
      r2a0=rnd(2)*a0
      suma=0.d0
+     pold=p
      s1: do mu=1,m
        suma=suma+a(mu)
        if(suma>=r2a0) exit s1
@@ -63,19 +67,32 @@ big: do inran=1,nran
      p(pr(mu))=p(pr(mu))+1
      cont(mu)=cont(mu)+1
      tcont=tcont+1
+!Test of equilibrium
+     eq: do j=1,m
+        sigold(j)=sig(j)
+        if(p(j)>pold(j)) sig(j)=p(j)
+        if(p(j)<pold(j)) sig(j)=-p(j)
+        if(sig(j)*sigold(j)<0) then
+           rep(j)=rep(j)+1
+           if(rep(j)==1000) exit inner
+        endif
+     enddo eq
+!Test of equilibrium
      do j=1,m
         a(j)=rate(j)*p(re(j))
      enddo
      a0=sum(a)
-     if(mod(tcont,tint)==0.or.a0==0) print "(e10.4,9999(i7))",t,p
+     if(mod(tcont,tint)==0.or.a0==0) print "(e10.4,9999(i8))",t,p 
+   enddo inner
+!Printing final populations
+   print*,"Population of every species"
+   do i=1,nesp
+      print "(i6,i8)",i,p(i)
    enddo
-enddo big
-print*,"Population of every species"
-do i=1,nesp
-   print "(i6,i7)",i,p(i)
-enddo
-print*,"counts per process"
-do i=1,m
-   print "(i6,i20)",i,cont(i)
-enddo
+!Printing statistics
+   print*,"counts per process"
+   do i=1,m
+      print "(i6,i20)",i,cont(i)
+   enddo
+enddo outter
 end program kmc
