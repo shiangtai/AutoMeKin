7235a9c98576fdf0cf5b01d9bbe23ec62c5a8177
tsscds <tsscds@4b1a781e-1fcd-455b-a5aa-50a18b27795a>
2018-12-19 22:18:30 +0000

    run_test.sh added

---
+++ scripts/run_test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Run this script to test the program for the formic acid example
+# You must have loaded tsscds module before:
+# module load tsscds/2018
+
+# Change the values below if needed:
+# path_to_program: is the path to the installation directory ($HOME/tsscds-2018 by default)
+# ntasks: is number of parallel tasks
+# niter: is the number of iterations
+
+path_to_program=$HOME/tsscds-2018
+ntasks=4
+niter=2
+runningtasks=$ntasks
+
+mkdir test
+cd test
+cp $path_to_program/examples/FA.* . 
+
+nohup llcalcs.sh FA.dat $ntasks $niter $runningtasks >llcalcs.log 2>&1 &
+
