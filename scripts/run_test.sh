14c2d4b2cfb12def684f400bd8dbec809dfb4b60
emilio <emilio@4b1a781e-1fcd-455b-a5aa-50a18b27795a>
2019-07-01 14:14:43 +0000

    run_test.sh updated

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -3,19 +3,31 @@
 # You must have loaded amk module before:
 # module load amk/2018
 
-# Change the values below if needed:
+# run_test.sh --prefix=path_to_program
 # path_to_program: is the path to the installation directory ($HOME/amk-2018 by default)
 # ntasks: is number of parallel tasks
 # niter: is the number of iterations
 
-path_to_program=$HOME/amk-2018
+if [ $# -ge 1 ]; then
+   tpath=${@: -1}
+   path_to_program=$(echo $tpath | sed  's/=/ /' | awk '{print $NF"/examples"}' | sed 's/\/\//\//')
+else
+   path_to_program=$HOME/amk-2018/examples
+fi
+file=$path_to_program/FA
+
+if [ ! -f ${file}.dat ]; then
+   echo "The path to amk-2018 is not correct"
+   exit
+fi
 ntasks=4
 niter=2
 runningtasks=$ntasks
 
-mkdir test
-cd test
-cp $path_to_program/examples/FA.* . 
+mkdir FA_test
+cd FA_test
+cp ${file}.dat ${file}.xyz .
 
 nohup llcalcs.sh FA.dat $ntasks $niter $runningtasks >llcalcs.log 2>&1 &
+
 
